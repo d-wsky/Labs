459a416f89f7ce1722fc04c5e9da58ed8f81a8c7
Denis Vasilkovsky <d.wsky@yandex.ru>
2015-08-12 02:12:32 +0300

    Улучшен пример LCD_4bit_WriteOnly и выделена библиотека HD44780.
    Теперь код для LCD-контроллеров перенесен в папку Common и на
    следующем коммите другие примеры смогут его использовать. Кроме
    того, функция инициализации теперь корректно работает с 4-х и
    8-ми битными интерфейсами дисплеев HD44780 и WS0001.

---
+++ Common/hd44780.c
@@ -0,0 +1,149 @@
+﻿/*
+ * hd44780.c
+ *
+ * Created: 11-Aug-15 23:45:48
+ *  Author: Денис Васильковский
+ */ 
+
+#include "hd44780.h"
+#include <util/delay.h>
+
+/* функция передачи тетрады в жки. передается
+младшая тетрада входного байта */
+void hd44780_putnibble(char t) {
+    t <<= HD44780_DATA_PIN;
+    HD44780_E_SET;
+	const char port_mask1 = (1 << HD44780_DATA_PIN) - 1;
+	const char port_mask2 = (1 << (HD44780_DATA_PIN + HD44780_DATA_WIDTH)) - 1;
+    HD44780_DATA_PORT &=~(port_mask1 ^ port_mask2);
+    HD44780_DATA_PORT |= t;
+    HD44780_E_CLR;
+    HD44780_PUTNIBBLE_DELAY;
+}
+
+/* функция передачи байта в жки. 
+char c - сам байт
+char rs - переменная, указывающая что передается:
+     rs = 0 - команда (устанавливается линия RS)
+	 rs = 1 - данные (сбрасывается линия RS) */
+void hd44780_putbyte(char c, char rs) {
+	if (rs == HD44780_COMMAND)
+		HD44780_RS_CLR;
+	else
+		HD44780_RS_SET;
+	#if (HD44780_DATA_WIDTH == 4)
+    hd44780_putnibble(c >> 4);
+    hd44780_putnibble(c);
+	#else
+    HD44780_E_SET;
+    HD44780_DATA_PORT = c;
+    HD44780_E_CLR;
+    HD44780_PUTNIBBLE_DELAY;
+	#endif
+}
+
+/* функция очистки дисплея и возврата
+курсора в начальную позицию*/
+void hd44780_clear() {
+	hd44780_putbyte(0x01, HD44780_COMMAND);
+	_delay_us(1500);
+}
+
+/* функция перемещения курсора в заданную позицию
+col - номер знакоместа по горизонтальной оси (от 0 до 15)
+row - номер строки (0 или 1) */
+void hd44780_gotoxy(char col, char row) {
+	char adr = 0x40 * (row & 1) + col;
+	adr |= HD44780_GOTO_CMD;
+	hd44780_putbyte(adr, HD44780_COMMAND);
+}
+
+/* функция перемещения курсора в начальную
+позицию (0, 0) */
+void hd44780_home() {
+	hd44780_putbyte(0x02, HD44780_COMMAND);
+}
+
+static void port_init() {
+	const char port_mask1 = (1 << HD44780_DATA_PIN) - 1;
+	const char port_mask2 = (1 << (HD44780_DATA_PIN + HD44780_DATA_WIDTH)) - 1;
+	volatile unsigned char* data_ddr = &HD44780_DATA_PORT - 1;
+	*data_ddr |= port_mask1 ^ port_mask2;
+	HD44780_DATA_PORT &=~(port_mask1 ^ port_mask2);
+	
+	volatile unsigned char* rs_ddr = &HD44780_RS_PORT - 1;
+	*rs_ddr |= 1 << HD44780_RS_PIN;
+	HD44780_RS_PORT &=~(1 << HD44780_RS_PIN);
+	
+	volatile unsigned char* e_ddr = &HD44780_E_PORT - 1;
+	*e_ddr |= 1 << HD44780_E_PIN;
+	HD44780_E_PORT &=~(1 << HD44780_E_PIN);
+	
+	volatile unsigned char* rw_ddr = &HD44780_RW_PORT - 1;
+	*rw_ddr |= 1 << HD44780_RW_PIN;
+	HD44780_RW_PORT &=~(1 << HD44780_RW_PIN);
+}
+
+/* функция инициализации работы жки
+в 4-битном режиме, без курсора */
+void hd44780_init(Hd44780PhysicalParams_t ph, 
+              Hd44780OutParams_t out,
+              Hd44780CursorMode_t cur) {
+	port_init();
+	_delay_ms(100);
+	// Передача команды сброса дисплея WS0001
+	HD44780_RS_CLR;
+	hd44780_putnibble(0x00);
+	_delay_ms(5);
+	hd44780_putnibble(0x00);
+	_delay_ms(5);
+	hd44780_putnibble(0x00);
+	_delay_ms(5);
+	hd44780_putnibble(0x00);
+	_delay_ms(5);
+	hd44780_putnibble(0x00);
+	_delay_ms(5);
+	
+	// Выбор шины, дисплея
+	#if (HD44780_DATA_WIDTH == 4)
+	hd44780_putnibble(ph >> 4);
+	_delay_ms(4.1);
+	hd44780_putnibble(ph >> 4);
+	_delay_us(100);
+	hd44780_putnibble(ph & 0x0f);
+	// Здесь должна быть проверка флага занятости
+	_delay_ms(10);
+	#else
+	hd44780_putbyte(ph, HD44780_COMMAND);
+	_delay_ms(4.1);
+	hd44780_putbyte(ph, HD44780_COMMAND);
+	_delay_us(100);
+	// Здесь должна быть проверка флага занятости
+	_delay_ms(10);
+	#endif	
+	
+	// Display ON/OFF Control
+	hd44780_putbyte(CursorOff, HD44780_COMMAND);
+	// Здесь должна быть проверка флага занятости
+	_delay_ms(10);
+	
+	// Очистка дисплея
+	hd44780_clear();
+	// Здесь должна быть проверка флага занятости
+	_delay_ms(10);
+
+	// Режим вывода
+	hd44780_putbyte(out, HD44780_COMMAND);
+	
+	// Дополнительно: режим работы курсора
+	hd44780_putbyte(cur, HD44780_COMMAND);
+	
+	// Дополнительно: возврат в координату (0, 0)
+	hd44780_home();
+}
+
+void hd44780_puts(char* str) {
+	while (*str) {
+		hd44780_putc(*str++);
+	}
+}
---
+++ Common/hd44780.h
@@ -0,0 +1,134 @@
+﻿/*
+ * hd44780.h
+ *
+ * Created: 11-Aug-15 23:38:56
+ *  Author: Денис Васильковский
+ *
+ *   About: Интерфейсный файл для библиотеки ЖКИ контроллера HD44780 и
+ *          совместимого с ним OLED контроллера Winstar.
+ *
+ *    Todo: Не поддерживается режим чтения данных из дисплея.
+ */ 
+
+
+#ifndef HD44780_H_
+#define HD44780_H_
+
+#include <avr/io.h>
+
+/* эти параметры зависят от аппаратной конфигурации */
+#define HD44780_RS_PIN              PC0
+#define HD44780_RS_PORT             PORTC
+#define HD44780_RW_PIN              PC2
+#define HD44780_RW_PORT             PORTC
+#define HD44780_E_PIN               PC1
+#define HD44780_E_PORT              PORTC
+#define HD44780_DATA_PORT           PORTC
+#define HD44780_DATA_PIN            PC4
+#define HD44780_DATA_WIDTH          4
+
+/* установка лог. "1" на линии E */
+#define HD44780_E_SET             ( HD44780_E_PORT  |= (1 << HD44780_E_PIN) )
+/* установка лог. "0" на линии E */
+#define HD44780_E_CLR             ( HD44780_E_PORT  &=~(1 << HD44780_E_PIN) )
+/* установка лог. "1" на линии RS */
+#define HD44780_RS_SET            ( HD44780_RS_PORT |= (1 << HD44780_RS_PIN) )
+/* установка лог. "0" на линии RS */
+#define HD44780_RS_CLR            ( HD44780_RS_PORT &=~(1 << HD44780_RS_PIN) )
+/* задержка на передачу данных в контроллер ЖКИ */
+#define HD44780_PUTNIBBLE_DELAY     _delay_us(50)
+
+/* макрос, указывающий функции, что
+передаются команды */
+#define HD44780_COMMAND             0
+/* макрос, указывающий функции, что
+передаются данные */
+#define HD44780_DATA                1
+
+/* типы данных, используемые для инициализации дисплея */
+
+/* физические параметры дисплея */
+#define HD44780_PHYSICAL_CMD        (1 << 5)
+#define HD44780_5x8MATRIX           (0 << 2)
+#define HD44780_5x10MATRIX          (1 << 2)
+#define HD44780_1ROW                (0 << 3)
+#define HD44780_2ROW                (1 << 3)
+#define HD44780_4BIT_BUS            (0 << 4)
+#define HD44780_8BIT_BUS            (1 << 4)
+/* поддержка русской кодовой страницы WS0001 */
+#define HD44780_RUS_FONT_TABLE      (2 << 0)
+typedef enum {
+	Phys8Bit1Row5x8  = HD44780_PHYSICAL_CMD | HD44780_8BIT_BUS | HD44780_1ROW | HD44780_5x8MATRIX  | HD44780_RUS_FONT_TABLE,
+	Phys8Bit1Row5x10 = HD44780_PHYSICAL_CMD | HD44780_8BIT_BUS | HD44780_1ROW | HD44780_5x10MATRIX | HD44780_RUS_FONT_TABLE,
+	Phys8Bit2Row5x8  = HD44780_PHYSICAL_CMD | HD44780_8BIT_BUS | HD44780_2ROW | HD44780_5x8MATRIX  | HD44780_RUS_FONT_TABLE,
+	Phys8Bit2Row5x10 = HD44780_PHYSICAL_CMD | HD44780_8BIT_BUS | HD44780_2ROW | HD44780_5x10MATRIX | HD44780_RUS_FONT_TABLE,
+	Phys4Bit1Row5x8  = HD44780_PHYSICAL_CMD | HD44780_4BIT_BUS | HD44780_1ROW | HD44780_5x8MATRIX  | HD44780_RUS_FONT_TABLE,
+	Phys4Bit1Row5x10 = HD44780_PHYSICAL_CMD | HD44780_4BIT_BUS | HD44780_1ROW | HD44780_5x10MATRIX | HD44780_RUS_FONT_TABLE,
+	Phys4Bit2Row5x8  = HD44780_PHYSICAL_CMD | HD44780_4BIT_BUS | HD44780_2ROW | HD44780_5x8MATRIX  | HD44780_RUS_FONT_TABLE,
+	Phys4Bit2Row5x10 = HD44780_PHYSICAL_CMD | HD44780_4BIT_BUS | HD44780_2ROW | HD44780_5x10MATRIX | HD44780_RUS_FONT_TABLE,
+} Hd44780PhysicalParams_t;
+
+/* направление вывода */
+#define HD44780_OUT_CMD             (1 << 2)
+#define HD44780_OUT_LEFT_TO_RIGHT   (1 << 1)
+#define HD44780_OUT_RIGHT_TO_LEFT   (0 << 1)
+typedef enum {
+	OutNorm   = HD44780_OUT_CMD | HD44780_OUT_LEFT_TO_RIGHT,
+	OutArabic = HD44780_OUT_CMD | HD44780_OUT_RIGHT_TO_LEFT
+} Hd44780OutParams_t;
+
+/* варианты отображения курсора */
+#define HD44780_CURSOR_MODE_CMD     (1 << 3)
+#define HD44780_DISPLAY_ON          (1 << 2)
+#define HD44780_DISPLAY_OFF         (0 << 2)
+#define HD44780_CURSOR_ON           (1 << 1)
+#define HD44780_CURSOR_OFF          (0 << 1)
+#define HD44780_BLINK_ON            (1 << 0)
+#define HD44780_BLINK_OFF           (0 << 0)
+
+typedef enum {
+	DispOff          = HD44780_CURSOR_MODE_CMD | HD44780_DISPLAY_OFF,
+	CursorOff        = HD44780_CURSOR_MODE_CMD | HD44780_DISPLAY_ON | HD44780_CURSOR_OFF | HD44780_BLINK_OFF,
+	CursorMode1      = HD44780_CURSOR_MODE_CMD | HD44780_DISPLAY_ON | HD44780_CURSOR_ON  | HD44780_BLINK_OFF,
+	CursorMode1Blink = HD44780_CURSOR_MODE_CMD | HD44780_DISPLAY_ON | HD44780_CURSOR_ON  | HD44780_BLINK_ON,
+	CursorMode2      = HD44780_CURSOR_MODE_CMD | HD44780_DISPLAY_ON | HD44780_CURSOR_OFF | HD44780_BLINK_OFF,
+	CursorMode2Blink = HD44780_CURSOR_MODE_CMD | HD44780_DISPLAY_ON | HD44780_CURSOR_OFF | HD44780_BLINK_ON
+} Hd44780CursorMode_t;
+
+#define HD44780_GOTO_CMD            (1 << 7)
+
+/* функция передачи байта в жки. 
+char c - сам байт
+char rs - переменная, указывающая что передается:
+     rs = 0 - команда (устанавливается линия RS)
+	 rs = 1 - данные (сбрасывается линия RS) */
+void hd44780_putbyte(char c, char rs);
+
+/* функция очистки дисплея и возврата
+курсора в начальную позицию*/
+void hd44780_clear();
+
+/* функция перемещения курсора в заданную позицию
+col - номер знакоместа по горизонтальной оси (от 0 до 15)
+row - номер строки (0 или 1) */
+void hd44780_gotoxy(char col, char row);
+
+/* функция перемещения курсора в начальную
+позицию (0, 0) */
+void hd44780_home();
+
+/* функция инициализации работы жки
+в 4-битном режиме, без курсора */
+void hd44780_init(Hd44780PhysicalParams_t ph,
+                  Hd44780OutParams_t out,
+                  Hd44780CursorMode_t cur);
+			  
+/* функция вывода отдельного байта на текущей позиции */
+static inline void hd44780_putc(char x) {
+	hd44780_putbyte(x, HD44780_DATA);
+}
+
+/* функция вывода стандартной строки на текущей позиции */
+void hd44780_puts(char * str);
+
+#endif /* HD44780_H_ */
