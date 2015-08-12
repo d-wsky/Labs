5b9888b52a41aa81447d5f41e61db4471cbc2496
Denis Vasilkovsky <d.wsky@yandex.ru>
2015-08-13 01:37:53 +0300

    Код, связанный с I2C выделен в отдельный модуль.
    И он теперь честно отрабатывает ошибки и готов к документированию
    в doxygen.

--- I2C_RTC_Example/I2C_RTC_Read_Example.c
+++ I2C_RTC_Example/I2C_RTC_Read_Example.c
@@ -16,91 +16,31 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "hd44780.h"
+#include "i2c.h"
 
 /* Адрес микросхемы PCF8583 на шине I2C */
 #define RTC_ADDR  0b10100000
 
-/* Передача команды СТАРТ на шину */
-void I2C_StartCondition(void)
-{
-    /* Передача команды */
-	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
-	
-	/* Ожидание установки флага TWINT. Это будет
-	означать окончание установки состояния СТАРТ */
-	while (!(TWCR & (1<<TWINT)));
-
-}
-
-/* Передача байта по шине с установлением
-подтверждения (ACK)*/
-void I2C_SendByte(unsigned char c)
-{
-	/* Запись данных в регистр TWDR. Очистка бита
-	TWINT для начала передачи */
-    TWDR = c;
-    TWCR = (1<<TWINT)|(1<<TWEN);
-	
-	/* Ожидание установки флага TWINT. Это будет
-	означать окончание передачи и установку
-	подтверждения (ACK) */
-	while (!(TWCR & (1<<TWINT)));
-}
-
-/* Получение байта */
-unsigned char I2C_RecieveByte(void)
-{
-	unsigned char dat;
-    
-	/* Запуск процесса */
-	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWEA);
-
-	/* Ожидание установки флага TWINT, говорящем
-	об окончании передачи.*/
-	while (!(TWCR & (1<<TWINT)));
-	
-	/* Считывание данных */
-	dat = TWDR;
-	return dat;
-}
-
-/* Получение байта без установки подтверждения */
-unsigned char I2C_RecieveLastByte(void)
-{
-	unsigned char dat;
-	/* Запуск процесса */
-	TWCR = (1<<TWINT)|(1<<TWEN)|(0<<TWEA);
-
-	/* Ожидание установки флага TWINT, говорящем
-	об окончании передачи.*/
-	while (!(TWCR & (1<<TWINT)));
-	
-	dat = TWDR;
-	return dat;
-}
-
-/* Передача состояния СТОП на шину */
-void I2C_StopCondition(void) {
-	TWCR = (1<<TWINT)|(1<<TWSTO)|(1<<TWEN);
-}
+#define I2C_FREQ  100000
 
 /* Глобальные переменные, хранящие текущее время и дату */
-static unsigned char sec, min, hour,
-                     year_date, day_month;
+typedef struct {
+	unsigned char sec;
+	unsigned char min;
+	unsigned char hour;
+	unsigned char year_date;
+	unsigned char day_month;
+} time_t ;
 
 /* Считывание текущего времени из микросхемы I2C */
-void get_time(void) {
-	I2C_StartCondition();
-	I2C_SendByte(RTC_ADDR);
-	I2C_SendByte(0x02);
-	I2C_StartCondition();
-	I2C_SendByte(RTC_ADDR|1);
-	sec=I2C_RecieveByte();
-	min=I2C_RecieveByte();
-	hour=I2C_RecieveByte();
-	year_date=I2C_RecieveByte();
-	day_month=I2C_RecieveLastByte();
-	I2C_StopCondition();
+void get_time(time_t* t) {
+	i2c_start();
+	i2c_write_addr(RTC_ADDR);
+	i2c_write_byte(0x02);
+	i2c_start();
+	i2c_write_addr(RTC_ADDR|1);
+	i2c_read_multiple((unsigned char*)t, sizeof(time_t));
+	i2c_stop();
 }
 
 /* переводит входное число в формате BCD в сторку
@@ -111,28 +51,28 @@ void bcd_to_str(uint8_t num, char* str) {
 }
 
 /* Функция вывода времени на ЖКИ */
-void put_time(void) {
+void put_time(time_t* t) {
 	/* Первая строка: время */
 	hd44780_gotoxy(0,0);
 	char buf[4] = {' ', ' ', ':', '\0'};
 	/* Часы */
-	bcd_to_str(hour, buf);
+	bcd_to_str(t->hour, buf);
 	hd44780_puts(buf);
 	/* Минуты */
-	bcd_to_str(min, buf);
+	bcd_to_str(t->min, buf);
 	hd44780_puts(buf);
 	/* Секунды */
-	bcd_to_str(sec, buf);
+	bcd_to_str(t->sec, buf);
 	buf[2] = '\0';
 	hd44780_puts(buf);
 	/* Вторая строка: дата */
 	hd44780_gotoxy(0, 1);
 	buf[2] = '/';
 	/* Число */
-	bcd_to_str(year_date & 0x03, buf);
+	bcd_to_str(t->year_date & 0x03, buf);
 	hd44780_puts(buf);
 	/* Месяц */
-	bcd_to_str(day_month & 0x03, buf);
+	bcd_to_str(t->day_month & 0x03, buf);
 	buf[2] = '\0';
 	hd44780_puts(buf);
 }
@@ -142,19 +82,17 @@ int main(void)
 	hd44780_init(Phys2Row5x8, OutNorm, CursorMode2);
 	hd44780_clear();
 	
-	/* Инициализация обмена по TWI на 100 кГц */
-    TWBR=0x2A;
-    /* Инициализация адреса МК на шине I2C */
-    TWAR=0x00;
+	i2c_init(I2C_FREQ);
     
     while (1) {
+		time_t t;
 		/* Считывание времени */
-		get_time();
+		get_time(&t);
 		
 		/* Вывод времени в формате:
 		 HH:MM:SS
 		 DD/MM */
-		put_time();
+		put_time(&t);
 		
 		_delay_ms(500);
 	};
