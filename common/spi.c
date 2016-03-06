d604164a07f176999d1d130d45aa46381c446b11
Denis Vasilkovskii <d.v.vasilkovsky@gmail.com>
2016-03-06 21:40:20 +0300

    Revert "Драйвер портов ввода-вывода и обновленный код примера SPI, использующий этот драйвер."
    This reverts commit 7832a7d145b6ab1f8235494e704f0d92e4584bbf.

--- common/spi.c
+++ common/spi.c
@@ -6,24 +6,20 @@
  */ 
 
 #include <avr/io.h>
-#include "gpio.h"
 #include "spi.h"
 
-/* Вспомогательный макрос, который вернет ненулевое значение,
-когда завершится текущая приемо-передача.*/
-#define TRX_COMPLETE   ( SPSR & (1 << SPIF) )
-
-void spi_init(SpiConfig_t * config) {
-	/* Установка выводов MOSI и SCK на вывод */
-	gpio_pin_mode_out(SPI_PORT, (1 << SPI_MOSI_PIN) | (1 << SPI_SCK_PIN));
+void spi_init(SpiConfig_t* config) {
+    /* Установка выводов MOSI и SCK на вывод */
+    DDRB = (1 << SPI_MOSI_PIN) | (1 << SPI_SCK_PIN);
 	/* Если использован режим мастера, то нужно ножку SS либо
 	переключить на вывод, либо поддерживать высокий уровень.
 	В противном случае автоматически включиться режим ведомого*/
 	if (config->master == SPI_MASTER_MODE) {
-		gpio_pin_mode_out(SPI_PORT, (1 << SPI_SS_PIN));
+		DDRB |= (1 << SPI_SS_PIN);
 	}
-	/* Включение запрошенного режима SPI */
-	SPCR = (config->mode) | (config->clk) | (config->order) | (config->master);
+    /* Включение SPI, режима ведущего, и установка
+	частоты тактирования fclk/128 */
+    SPCR = (config->mode)|(config->clk)|(config->order)|(config->master);
 }
 
 /* Функция передачи байта данных outData и приема возвратного байта.*/
@@ -31,8 +27,6 @@ unsigned char spi_write_blocking(char outData) {
 	/* Начало передачи */
 	SPDR = outData;
 	/* Ожидание окончания передачи */
-	while (!TRX_COMPLETE) {
-		// Пустой цикл
-	}
+	while(!(SPSR & (1<<SPIF))) ;
 	return SPDR;
 }
