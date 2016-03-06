2c484c21854b0012d01366d4249442d3037e85a2
Denis Vasilkovsky <d.wsky@yandex.ru>
2016-03-06 21:52:59 +0300

    Драйвер портов ввода-вывода и обновленный код примера SPI, использующий этот драйвер.
    Теперь использован правильный username в git.

--- SPI_ADC_Example/SPI_ADC_Example.c
+++ SPI_ADC_Example/SPI_ADC_Example.c
@@ -8,7 +8,7 @@
  *          интерфейсу SPI. Также как и в примере со встроенным АЦП, полученные
  *          данные отображаются на портах C и B в двоичном виде. Не забываем,
  *          что для обеспечения связи с этой микросхемой необходимо установить
- *          правильные положения переключателей MOSI, MISO, SCK на плате
+ *          правильные положения переключателей MOSI, MISO, SCK, ADC-CS на плате
  *          UNI-DS3.
  *
  *    Note: На плате должны быть включены переключатели ADC-CS, MOSI, MISO, SCK.
@@ -18,6 +18,7 @@
 #define F_CPU 10000000UL
 #include <util/delay.h>
 
+#include "gpio.h"
 #include "spi.h"
 #include "mcp3204.h"
 
@@ -25,8 +26,8 @@ int main(void) {
 	/* 0. Подготовка используемых средств к работе.*/
 	
 	/* На эти ножки выводится результат преобразования.*/
-	DDRC = 0xFF;
-	DDRD = 0xFF;
+	gpio_pin_mode_out(GPIO_PORTC, GPIO_PIN_ALL);
+	gpio_pin_mode_out(GPIO_PORTD, GPIO_PIN_ALL);
 	
 	/* Подготовка модуля интерфейса SPI к работе.*/
 	SpiConfig_t spi_config = {
@@ -42,15 +43,15 @@ int main(void) {
 	/* Инициализация микросхемы АЦП.*/
 	mcp3204_init();
 	
-    while(1) {
+	while(1) {
 		/* 1. Чтение данных.*/
 		uint16_t adc_result = mcp3204_read(MCP3204_CHANNEL_CH1);
 
 		/* 2. Отображение данных.*/
-		PORTC = adc_result;
-		PORTD = adc_result >> 8;
+		gpio_port_assign(GPIO_PORTC, adc_result);
+		gpio_port_assign(GPIO_PORTD, adc_result >> 8);
 		
 		/* 3. Пауза.*/
 		_delay_ms(100);
-    }
+	}
 }
--- common/spi.c
+++ common/spi.c
@@ -6,20 +6,24 @@
  */ 
 
 #include <avr/io.h>
+#include "gpio.h"
 #include "spi.h"
 
-void spi_init(SpiConfig_t* config) {
-    /* Установка выводов MOSI и SCK на вывод */
-    DDRB = (1 << SPI_MOSI_PIN) | (1 << SPI_SCK_PIN);
+/* Вспомогательный макрос, который вернет ненулевое значение,
+когда завершится текущая приемо-передача.*/
+#define TRX_COMPLETE   ( SPSR & (1 << SPIF) )
+
+void spi_init(SpiConfig_t * config) {
+	/* Установка выводов MOSI и SCK на вывод */
+	gpio_pin_mode_out(SPI_PORT, (1 << SPI_MOSI_PIN) | (1 << SPI_SCK_PIN));
 	/* Если использован режим мастера, то нужно ножку SS либо
 	переключить на вывод, либо поддерживать высокий уровень.
 	В противном случае автоматически включиться режим ведомого*/
 	if (config->master == SPI_MASTER_MODE) {
-		DDRB |= (1 << SPI_SS_PIN);
+		gpio_pin_mode_out(SPI_PORT, (1 << SPI_SS_PIN));
 	}
-    /* Включение SPI, режима ведущего, и установка
-	частоты тактирования fclk/128 */
-    SPCR = (config->mode)|(config->clk)|(config->order)|(config->master);
+	/* Включение запрошенного режима SPI */
+	SPCR = (config->mode) | (config->clk) | (config->order) | (config->master);
 }
 
 /* Функция передачи байта данных outData и приема возвратного байта.*/
@@ -27,6 +31,8 @@ unsigned char spi_write_blocking(char outData) {
 	/* Начало передачи */
 	SPDR = outData;
 	/* Ожидание окончания передачи */
-	while(!(SPSR & (1<<SPIF))) ;
+	while (!TRX_COMPLETE) {
+		// Пустой цикл
+	}
 	return SPDR;
 }
